0f13668c00db41ed7e11f9735afafb46437d512f
mrichter <mark.richter@thoughtworks.com>
2011-12-21 08:42:34 -0800

    fix gem_path and path

--- buildvars.bat
+++ buildvars.bat
@@ -1,5 +1,5 @@
 set DOTNETPATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319\
 set VSPATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\
 set CONFIG=Release
-set GEM_PATH=vendor\ironruby\1.8
-set PATH=vendor\ironruby\1.8\bin;tools\ironruby\bin;%PATH%
+set GEM_PATH=vendor\bundle\ironruby\1.8
+set PATH=vendor\bundle\ironruby\1.8\bin;tools\ironruby\bin;%PATH%
